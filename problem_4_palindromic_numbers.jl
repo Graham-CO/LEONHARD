d6b029df46568fdd067d6d77d122e9dd4f5fa9ae
Graham-CO <grwi2594@colorado.edu>
2020-10-28 09:02:59 -0600

    finds the largest palindrome of 2 3 digit numbers

---
+++ problem_4_palindromic_numbers.jl
@@ -0,0 +1,16 @@
+function largestPalindrome()
+    x = []
+    for i in 100:999, j in 100:999
+        z = i*j
+        if string(z) == reverse(string(z)) 
+            push!(x,z)
+        end
+    end
+    println(findmax(x))
+end
+
+largestPalindrome()
+
+
+
+
