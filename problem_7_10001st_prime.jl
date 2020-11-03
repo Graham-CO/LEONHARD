d3e2b0e10a2db6e4f703a4364b984586035606eb
Graham-CO <grwi2594@colorado.edu>
2020-11-03 11:16:12 -0700

    Commented isPrime

--- problem_7_10001st_prime.jl
+++ problem_7_10001st_prime.jl
@@ -1,16 +1,16 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 
-function pushPrime()
-    i = 3
+function pushPrime() 
+    i = 2
     z = []
-    while length(z) < 10001
-        if isPrime(i) == true
+    while length(z) < 10002
+        if isPrime(i) == 1
             push!(z,i)
             i += 1
         end
     end
-    println(z)
+    println(last(z))
 end
 
 #function isPrime(n)
@@ -24,14 +24,14 @@ end
     #end
 #end
 
-function isPrime(n)
-    x::Bool = true
-    for j in 2:sqrt(n)
+function isPrime(n) #Checks if prime or not, should pass i from pushPrime() to it 
+    x = 1 #initialize x as 1 becuase im not smart enough for bools yet
+    for j in 2:sqrt(n) # only check upto sqrt of the number, everything after is extraneous
         if n % j == 0
-            x = false
+            x = 0 #if not prime return 0 (false)
         end
     end
-    return x
+    return x #should return 1 or 0
 end
 
 pushPrime()
